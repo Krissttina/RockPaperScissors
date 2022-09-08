1811f185aaaa76570b4765dc60318409eda9259e
Krissttina <krismetodieva123@gmail.com>
2022-09-08 18:04:26 +0300

    saving progress

---
+++ script.js
@@ -0,0 +1,13 @@
+// Setting the scores and selecting our HTML elements.
+let computerScore = 1;
+let playerScore = 1;
+const pScore = document.getElementById('playerScore');
+const cScore = document.getElementById('computerScore');
+const buttons = document.querySelectorAll('.selection button');
+const showIcon = document.querySelector('.show i');
+const computerShowIcon = document.querySelector('.computer i');
+// The randomClass array below this contains the rock,paper, and scissor Icon from font-awesome.
+const randomClasses = ["fas fa-hand-rock", "fas fa-hand-paper","fas fa-hand-scissors"];
+const text = document.getElementById('demo');
+const text2 = document.getElementById('demo2');
+
