b0c49716b08e1749089aa149b13f2293b657a69c
Kristina <krismetodieva123@gmail.com>
2023-05-15 11:41:57 +0300

    update

--- README.md
+++ README.md
@@ -1,22 +1,10 @@
-# Rock Paper Scissors
+### Rock Paper Scissors
 
-## Content
-- [Intro](#intro)
-- [Description](#description)
-- [Image](#image)
-
-
-### Intro
-
-Rock paper scissors is one of the most popular games in the world. No matter the way it played the goal is always the same. 
-
-### Description
-
-The game is usually played be two players. These player count on three and choose one of three options. Exactly rock, paper or scissors.
+Rock paper scissors is one of the most popular games in the world.
 
 ### Rules
 
-The rules are very simple. When the players choose one of the options one of them is going to win depending on the choice. Rock beats the scissors but not the paper. And so on the paper beats the rock but not the scissors.
+The rules are very simple. When the players choose an options one of them is going to win depending on the choice. Rock beats the scissors but not the paper. And so the paper beats the rock but not the scissors.
 
 ### Images
-![stars](game(1).png)
+![preview](preview.png)
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Setting the scores and selecting our HTML elements.
+//Setting the scores and selecting our HTML elements.
 let computerScore = 1;
 let playerScore = 1;
 const pScore = document.getElementById('playerScore');
@@ -7,15 +7,14 @@ const buttons = document.querySelectorAll('.selection button');
 const showIcon = document.querySelector('.show i');
 const computerShowIcon = document.querySelector('.computer i');
 
-// The randomClass array below this contains the rock,paper, and scissor Icon from font-awesome.
+//The randomClass array below this contains the rock, paper, and scissor Icon
 const randomClasses = ["fas fa-hand-rock", "fas fa-hand-paper","fas fa-hand-scissors"];
-const text = document.getElementById('demo');
-const text2 = document.getElementById('demo2');
+const text = document.getElementById('luckText');
 
-// Game Functionality: Setting forEach function for the buttons.
-const game = () =>{
+// Game Functionality: Setting forEach function for the buttons
+const game = () => {
     buttons.forEach(btn =>{
-        btn.addEventListener('click',(e)=>{
+        btn.addEventListener('click',(e) => {
         // Random rock paper scissor for the computer and the player
            let clickedBtn = e.target.className;
            showIcon.className = clickedBtn;
@@ -24,67 +23,44 @@ const game = () =>{
            // Game Score.
            // If it's a Tie .
            if(showIcon.className === computerShowIcon.className){
-               pScore.innerHTML = pScore.innerHTML;
-               cScore.innerHTML = cScore.innerHTML;
-               text.innerHTML = "It's a Tie ! ";
+               text.innerHTML = "It's a Tie!";
                text.style.color = 'orange';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'orange';
            } 
-          // if it's not a Tie.
+          // If it's not a Tie.
            else if(showIcon.className === randomClasses[0] && computerShowIcon.className === randomClasses[2]){
                pScore.innerHTML = playerScore;
                playerScore++;
-               text.innerHTML = "It's a Win ! ";
+               text.innerHTML = "It's a Win!";
                text.style.color = 'rgb(1, 146, 1)';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'rgb(1, 146, 1)';
            }else if(showIcon.className === randomClasses[0] && computerShowIcon.className === randomClasses[1]){
                cScore.innerHTML = computerScore;
                computerScore++;
-               text.innerHTML = "You Loosed ! ";
+               text.innerHTML = "You Lost!";
                text.style.color = 'red';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'red';
            }else if(showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[2]){
                cScore.innerHTML = computerScore;
                computerScore++;
-               text.innerHTML = "You Loosed ! ";
+               text.innerHTML = "You Lost!";
                text.style.color = 'red';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'red';
            }else if(showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[0]){
                pScore.innerHTML = playerScore;
                playerScore++;
-               text.innerHTML = "It's a Win ! ";
+               text.innerHTML = "It's a Win!";
                text.style.color = 'rgb(1, 146, 1)';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'rgb(1, 146, 1)';
            }else if(showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[0]){
                cScore.innerHTML = computerScore;
                computerScore++;
-               text.innerHTML = "You Loosed ! ";
+               text.innerHTML = "You Lost!";
                text.style.color = 'red';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'red';
            }else if(showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[1]){
                pScore.innerHTML = playerScore;
                playerScore++;
-               text.innerHTML = "It's a Win ! ";
+               text.innerHTML = "It's a Win!";
                text.style.color = 'rgb(1, 146, 1)';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'rgb(1, 146, 1)';
            }
         });
     });
 }
 
-//let reset = document.getElementById('hiden');
-//Reset button
-/*buttons.addEventListener("click", () => {
-    reset.removeAttribute(hidden);
-});*/
-
 // Calling the function.
 game();
-// This function contains all the game logic.
